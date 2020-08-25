8164cbb61adef2bacf039fc9a788e224b6dfc996
Dart Au <dart.au@gammonconstruction.com>
2020-08-25 10:09:20 +0800

    demo

---
+++ dartau-demoapp/Controllers/GreetingController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace dartau_demoapp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GreetingController : ControllerBase
+    {
+        public string Get(int id) { return "Hello world"; }
+    }
+}
